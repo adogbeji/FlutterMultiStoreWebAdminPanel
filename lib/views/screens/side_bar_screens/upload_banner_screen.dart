71b7370f2ef160174b4d7a99463da25afd178f3f
adogbeji <baka_1993@yahoo.co.uk>
2024-06-15 09:18:01 +0100

    Start building upload banner screen

--- lib/views/screens/side_bar_screens/upload_banner_screen.dart
+++ lib/views/screens/side_bar_screens/upload_banner_screen.dart
@@ -7,16 +7,42 @@ class UploadBannerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        alignment: Alignment.topLeft,
-        padding: const EdgeInsets.all(10),
-        child: const Text(
-          'Upload Banners',
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 36,
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Banners',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 36,
+              ),
+            ),
           ),
-        ),
+
+          const Divider(color: Colors.grey,),
+
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: Container(
+                  height: 140,
+                  width: 140,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade500,
+                    border: Border.all(color: Colors.grey.shade800,),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Center(
+                    child: Text('Banners'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
