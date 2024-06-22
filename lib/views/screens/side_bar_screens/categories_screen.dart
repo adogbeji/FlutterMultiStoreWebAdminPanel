df848f676e881ef115a81ef461cd28664f291d5a
adogbeji <baka_1993@yahoo.co.uk>
2024-06-22 09:27:57 +0100

    Start building categories screen & add search bar

--- lib/views/screens/side_bar_screens/categories_screen.dart
+++ lib/views/screens/side_bar_screens/categories_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class CategoriesScreen extends StatelessWidget {
   // const CategoriesScreen({super.key});
@@ -7,16 +8,85 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        alignment: Alignment.topLeft,
-        padding: const EdgeInsets.all(10),
-        child: const Text(
-          'Categories',
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 36,
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Categories',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 36,
+              ),
+            ),
           ),
-        ),
+          const Divider(
+            color: Colors.grey,
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 140,
+                      width: 140,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade500,
+                        border: Border.all(
+                          color: Colors.grey.shade800,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Center(
+                        child: Text('Category'),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.yellow.shade900,
+                      ),
+                      child: const Text('Upload Image'),
+                    ),
+                  ],
+                ),
+              ),
+
+              // SEARCH BAR
+              Flexible(
+                child: SizedBox(
+                  width: 320,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Enter Category Name',
+                      hintText: 'Enter Category Name',
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(
+                width: 30,
+              ),
+              
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.yellow.shade900,
+                ),
+                child: const Text('Save'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
