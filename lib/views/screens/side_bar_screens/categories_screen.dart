ad1e961f6a2439710e7fe33c0b946f312f0e3659
adogbeji <baka_1993@yahoo.co.uk>
2024-06-22 09:38:33 +0100

    Add validation to seasrch bar in categories screen & add form key

--- lib/views/screens/side_bar_screens/categories_screen.dart
+++ lib/views/screens/side_bar_screens/categories_screen.dart
@@ -5,88 +5,99 @@ class CategoriesScreen extends StatelessWidget {
   // const CategoriesScreen({super.key});
   static const String routeName = '\CategoriesScreen';
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>(); // Form Key
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          Container(
-            alignment: Alignment.topLeft,
-            padding: const EdgeInsets.all(10),
-            child: const Text(
-              'Categories',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 36,
-              ),
-            ),
-          ),
-          const Divider(
-            color: Colors.grey,
-          ),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(14.0),
-                child: Column(
-                  children: [
-                    Container(
-                      height: 140,
-                      width: 140,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade500,
-                        border: Border.all(
-                          color: Colors.grey.shade800,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: const Center(
-                        child: Text('Category'),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.yellow.shade900,
-                      ),
-                      child: const Text('Upload Image'),
-                    ),
-                  ],
+      child: Form(
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.topLeft,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                'Categories',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 36,
                 ),
               ),
-
-              // SEARCH BAR
-              Flexible(
-                child: SizedBox(
-                  width: 320,
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Enter Category Name',
-                      hintText: 'Enter Category Name',
+            ),
+            const Divider(
+              color: Colors.grey,
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 140,
+                        width: 140,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade500,
+                          border: Border.all(
+                            color: Colors.grey.shade800,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Center(
+                          child: Text('Category'),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.yellow.shade900,
+                        ),
+                        child: const Text('Upload Image'),
+                      ),
+                    ],
+                  ),
+                ),
+        
+                // SEARCH BAR
+                Flexible(
+                  child: SizedBox(
+                    width: 320,
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Category Name Must Not Be Empty!';
+                        } else {
+                          return null;
+                        }
+                      },
+                      decoration: const InputDecoration(
+                        labelText: 'Enter Category Name',
+                        hintText: 'Enter Category Name',
+                      ),
                     ),
                   ),
                 ),
-              ),
-
-              const SizedBox(
-                width: 30,
-              ),
-              
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: Colors.yellow.shade900,
+        
+                const SizedBox(
+                  width: 30,
                 ),
-                child: const Text('Save'),
-              ),
-            ],
-          ),
-        ],
+        
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.yellow.shade900,
+                  ),
+                  child: const Text('Save'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
