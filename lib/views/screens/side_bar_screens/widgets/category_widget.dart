25af98877c50932f494583148432c51dd47284c8
adogbeji <baka_1993@yahoo.co.uk>
2024-06-28 09:48:52 +0100

    Add code to display uploaded categories in CategoryWidget

--- lib/views/screens/side_bar_screens/widgets/category_widget.dart
+++ lib/views/screens/side_bar_screens/widgets/category_widget.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class CategoryWidget extends StatelessWidget {
@@ -5,10 +6,43 @@ class CategoryWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Category Widget'),
-      ),
-    );
+    final Stream<QuerySnapshot> _categoriesStream = FirebaseFirestore.instance.collection('categories').snapshots();
+
+    return StreamBuilder(
+      stream: _categoriesStream,
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Something went wrong!');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(color: Colors.cyan,),
+          );
+        }
+
+        return GridView.builder(
+          itemCount: snapshot.data!.size,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 6,
+            mainAxisSpacing: 8,
+            crossAxisSpacing: 8,
+          ),
+          itemBuilder: (context, index) {
+            final categoryData = snapshot.data!.docs[index];
+            return Column(
+              children: [
+                SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Image.network(categoryData['image']),
+                ),
+
+                Text(categoryData['categoryName']),
+              ],
+            );
+          }
+          );
+      });
   }
 }
