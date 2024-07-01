5dacb16d8f55f31c4a1af7bf2b75cb789962b836
adogbeji <baka_1993@yahoo.co.uk>
2024-07-01 09:33:36 +0100

    Define new method in vendors screen

--- lib/views/screens/side_bar_screens/vendors_screen.dart
+++ lib/views/screens/side_bar_screens/vendors_screen.dart
@@ -1,8 +1,25 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class VendorScreen extends StatelessWidget {
   // const VendorScreen({super.key});
   static const String routeName = '\VendorScreen';
+
+  Widget _rowHeader(String text, int flex) {
+    return Expanded(
+      flex: flex,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.grey.shade700,
+          ),
+          color: Colors.yellow.shade700,
+        ),
+        child: Text(text),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
