60d0d207248aef9c7409056bef58b6debe6c85db
adogbeji <baka_1993@yahoo.co.uk>
2024-07-01 09:54:25 +0100

    Start building table header in vendors screen

--- lib/views/screens/side_bar_screens/vendors_screen.dart
+++ lib/views/screens/side_bar_screens/vendors_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 class VendorScreen extends StatelessWidget {
@@ -14,9 +12,12 @@ class VendorScreen extends StatelessWidget {
           border: Border.all(
             color: Colors.grey.shade700,
           ),
-          color: Colors.yellow.shade700,
+          color: Colors.yellow.shade900,
         ),
-        child: Text(text),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(text, style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold,),),
+        ),
       ),
     );
   }
@@ -24,16 +25,31 @@ class VendorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        alignment: Alignment.topLeft,
-        padding: const EdgeInsets.all(10),
-        child: const Text(
-          'Manage Vendors',
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 36,
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Manage Vendors',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 36,
+              ),
+            ),
           ),
-        ),
+
+          Row(
+            children: [
+              _rowHeader('LOGO', 1),
+              _rowHeader('BUSINESS NAME', 3),
+              _rowHeader('CITY', 2),
+              _rowHeader('STATE', 2),
+              _rowHeader('ACTION', 1),
+              _rowHeader('VIEW MORE', 1),
+            ],
+          ),
+        ],
       ),
     );
   }
