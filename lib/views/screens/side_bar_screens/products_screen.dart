8b013e7b1e2b15d0530c487e1950418dd445b629
adogbeji <baka_1993@yahoo.co.uk>
2024-07-03 09:26:49 +0100

    Add new method in products screen

--- lib/views/screens/side_bar_screens/products_screen.dart
+++ lib/views/screens/side_bar_screens/products_screen.dart
@@ -4,6 +4,24 @@ class ProductScreen extends StatelessWidget {
   // const ProductScreen({super.key});
   static const String routeName = '\ProductScreen';
 
+  Widget _rowHeader(String text, int flex) {
+    return Expanded(
+      flex: flex,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.grey.shade700,
+          ),
+          color: Colors.yellow.shade900,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(text, style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold,),),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
