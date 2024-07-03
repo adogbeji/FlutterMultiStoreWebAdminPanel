e3389bdae211b8451d32495e905c20b3c833dca8
adogbeji <baka_1993@yahoo.co.uk>
2024-07-03 09:33:28 +0100

    Start building table headers in products screen

--- lib/views/screens/side_bar_screens/products_screen.dart
+++ lib/views/screens/side_bar_screens/products_screen.dart
@@ -25,16 +25,31 @@ class ProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        alignment: Alignment.topLeft,
-        padding: const EdgeInsets.all(10),
-        child: const Text(
-          'Products',
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 36,
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Products',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 36,
+              ),
+            ),
           ),
-        ),
+
+          Row(
+            children: [
+              _rowHeader('IMAGE', 1),
+              _rowHeader('NAME', 3),
+              _rowHeader('PRICE', 2),
+              _rowHeader('QUANTITY', 2),
+              _rowHeader('ACTION', 1),
+              _rowHeader('VIEW MORE', 1),
+            ],
+          ),
+        ],
       ),
     );
   }
